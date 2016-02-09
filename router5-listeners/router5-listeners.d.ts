5a827550bca5263e81d1901a6ce85fa7ec15892b
Frank Laub <frank.laub@gmail.com>
2016-02-08 16:11:15 -0800

    Extend Router5

--- router5-listeners/router5-listeners.d.ts
+++ router5-listeners/router5-listeners.d.ts
@@ -4,3 +4,14 @@ declare module "router5-listeners" {
 	function listenersPlugin(options?: {});
 	export = listenersPlugin;
 }
+
+declare module 'router5' {
+	interface Router5 {
+		addListener(fn: Function);
+		removeListener(fn: Function);
+		addNodeListener(nodeName: string, fn: Function);
+		removeNodeListener(nodeName: string, fn: Function);
+		addRouteListener(routeName: string, fn: Function);
+		removeRouteListener(routeName: string, fn: Function);
+	}
+}
