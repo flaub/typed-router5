0f7f2787345c40b2415cf8e768fb01b8797d2af4
Frank Laub <frank.laub@gmail.com>
2016-02-28 18:02:06 -0800

    Adding react-fa

---
+++ react-fa/react-fa.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for react-fa v4.0.0
+// Project: https://github.com/andreypopp/react-fa
+// Definitions by: Frank Laub <https://github.com/flaub>
+
+declare module "react-fa" {
+	import { ComponentClass, CSSProperties, DOMAttributes, Props } from 'react';
+
+	interface IconProps extends DOMAttributes, Props<Icon> {
+		name: string;
+		className?: string;
+		style?: CSSProperties;
+		size?: string;
+		spin?: boolean;
+		rotate?: string;
+		flip?: string;
+		fixedWidth?: boolean;
+		pulse?: boolean;
+		stack?: string;
+		inverse?: boolean;
+	}
+
+	interface Icon extends ComponentClass<IconProps> { }
+	const Icon: Icon;
+
+	export = Icon;
+}
