05726f4bc799caff90b6d8bc9416b52db6ec7397
Frank Laub <frank.laub@gmail.com>
2016-03-05 12:04:20 -0800

    Upgrade router5-history and router5-listeners to v3.0.0

--- router5-history/router5-history.d.ts
+++ router5-history/router5-history.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for router5-history v2.0.0
+// Type definitions for router5-history v3.0.0
 
 declare module "router5-history" {
 	function historyPlugin(options?: {});
-	export = historyPlugin;
+	export default historyPlugin;
 }
--- router5-listeners/router5-listeners.d.ts
+++ router5-listeners/router5-listeners.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for router5-listeners v2.0.0
+// Type definitions for router5-listeners v3.0.0
 
 declare module "router5-listeners" {
 	function listenersPlugin(options?: {});
-	export = listenersPlugin;
+	export default listenersPlugin;
 }
 
 declare module 'router5' {
